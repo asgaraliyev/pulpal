9a623f2dc699b3d72dd35c507c697f280909fe04
asgaraliyev <asgaraliyev2001@gmail.com>
2022-03-04 19:56:37 +0400

    create payment url function added

--- index.js
+++ index.js
@@ -7,13 +7,20 @@ class PulPal {
         ${message}
     `);
   }
-  constructor({ mode = "DEV", defaultLng = "en", salt ,merchantId,customMerchantName}) {
+  constructor({
+    mode = "DEV",
+    defaultLng = "en",
+    salt,
+    merchantId,
+    customMerchantName,
+  }) {
     this.salt = salt;
     this.merchantId = merchantId;
-    this.customMerchantName=customMerchantName
+    this.customMerchantName = customMerchantName;
     if (!this.salt) this.throwError("salt not found");
     if (!this.merchantId) this.throwError("merchantId not found");
-    if (!this.customMerchantName) this.throwError("customMerchantName not found");
+    if (!this.customMerchantName)
+      this.throwError("customMerchantName not found");
     this.mode = mode;
     const paymentURLbyMode = {
       DEV: "https://pay-dev.pulpal.az/payment",
@@ -21,5 +28,61 @@ class PulPal {
     };
     this.paymentURL = paymentURLbyMode[this.mode];
     this.lng = defaultLng;
+  }
+  createSignature2(paymentData) {
+    let joined =
+      paymentData.name.en +
+      paymentData.name.az +
+      paymentData.name.ru +
+      paymentData.description.en +
+      paymentData.description.az +
+      paymentData.description.ru +
+      paymentData.merchantId +
+      paymentData.externalId +
+      paymentData.price;
+
+    let milliseconds = new Date().getTime();
+
+    let fromEpoch = Math.floor(milliseconds / 300000);
+    joined += fromEpoch + salt;
+    const hash = crypto.createHash("sha1").update(joined);
+    return hash.digest("hex");
+  }
+  objectToBase64(obj) {
+    return Buffer.from(JSON.stringify(obj)).toString("base64");
+  }
+  createPaymentURL({
+    price,
+    externalId,
+    repeatable,
+    name,
+    description,
+    lng = this.lng,
+  }) {
+    if (!price) return this.throwError("price field not found");
+    else if (!externalId) return this.throwError("externalId field not found");
+    else if (!repeatable) return this.throwError("repeatable field not found");
+    else if (!name) return this.throwError("name field not found");
+    else if (!description)
+      return this.throwError("description field not found");
+    else if (typeof name !== "object")
+      return this.throwError("name field has to be object");
+    else if (typeof description !== "object")
+      return this.throwError("description field has to be object");
+    let paymentData = {
+      merchantId: this.merchantId,
+      customMerchantName: this.customMerchantName,
+      externalId,
+      repeatable,
+      name,
+      description,
+      price,
+      signature2: null,
+    };
+    paymentData.signature2 = this.createSignature2(paymentData);
+    paymentData = this.objectToBase64(paymentData);
+    return `${this.paymentURL}/?lang=${lng}&paymentdata=${encodeURI(
+      paymentData
+    )}`;
   }
 }
